af0b25d4f5ea0c45906342fc4dcd489712ae0cd1
xepea <fopeak@gmail.com>
2020-02-20 20:51:04 +0300

    faster

--- dexif.go
+++ dexif.go
@@ -1,6 +1,7 @@
 package dexif
 
 import (
+	"encoding/binary"
 	"errors"
 	"io"
 	"os"
@@ -8,33 +9,31 @@ import (
 
 // Dexif @ removes exif data in image
 func Dexif(filepath string, destpath string) error {
+
 	f, err := os.Open(filepath)
-	defer f.Close()
 	if err != nil {
 		return err
 	}
-	buffer := make([]byte, 64000)
-	_, err = f.ReadAt(buffer, 0)
+	buffer := make([]byte, 4)
+	_, err = f.ReadAt(buffer, 2)
 	if err != nil {
 		return err
 	}
-	if buffer[2] == 0xFF && buffer[3] == 0xE1 {
-		for x := 1; x < 64000; x++ {
-			if buffer[x-1] == 0xFF && buffer[x] == 0xD9 {
-				fout, err := os.Create(destpath)
-				if err != nil {
-					return err
-				}
-				fout.Write([]byte{0xFF, 0xD8})
-				f.Seek(int64(x), 0)
-				_, err = io.Copy(fout, f)
-				if err != nil {
-					return err
-				}
-				defer fout.Close()
-				return nil
-			}
+
+	if buffer[0] == 0xFF && buffer[1] == 0xE1 {
+		exifSize := binary.BigEndian.Uint16(buffer[2:4])
+		exifSize += 4
+		fout, err := os.Create(destpath)
+		if err != nil {
+			return err
 		}
+		fout.Write([]byte{0xFF, 0xD8})
+		f.Seek(int64(exifSize), 0)
+		_, err = io.Copy(fout, f)
+		if err != nil {
+			return err
+		}
+		return nil
 	}
-	return errors.New("no exif data found")
+	return errors.New("exif data not found")
 }
