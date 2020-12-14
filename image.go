f55fd1ca776cd5128e630936c7e43e8d7f53fe05
Enes Furkan Olcay <fopeak@gmail.com>
2020-12-14 22:59:17 +0300

    Strip function now returning new image bytes

--- image.go
+++ image.go
@@ -12,10 +12,10 @@ func NewImage(buf []byte) *Image {
 	}
 }
 
-func (image *Image) Strip() error {
+func (image *Image) Strip() ([]byte, error) {
 	if image.buf[0] == 0xFF && image.buf[1] == 0xD8 {
 		jpeg := NewJPEG(image.buf)
-		jpeg.Strip()
+		return jpeg.Strip()
 	}
-	return errors.New("file format is not supported")
+	return nil, errors.New("file format is not supported")
 }
